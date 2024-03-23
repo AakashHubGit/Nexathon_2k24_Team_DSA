7da79094f24802ef4187bbe4b738c32baed49155
Devansh-Aage <devanshaage@gmail.com>
2024-03-23 20:59:36 +0530

    Routing

--- ghardhundo/src/App.js
+++ ghardhundo/src/App.js
@@ -2,21 +2,19 @@
 import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import PropertyForm from './components/PropertyForm';
-import SignUp from 'components/SignUp';
+import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 
 function App() {
   return (
     <>
+    <Navbar/>
       <Routes>
+        <Route path='/' element={<Home/>}/>
         { <Route path="/signup" element={<SignUp />} /> }
         { <Route path="/propertyform" element={<PropertyForm />}/>}
       </Routes>
-    {/* <div className="App">
-      <Navbar/>
-      <Home/>
-    </div> */}
     </>
   );
 }
