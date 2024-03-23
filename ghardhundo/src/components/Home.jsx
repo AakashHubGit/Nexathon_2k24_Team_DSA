92e90ebca8c024a064339141207f4f9d1d8019a6
Devansh-Aage <devanshaage@gmail.com>
2024-03-23 20:40:42 +0530

    Modal

--- ghardhundo/src/components/Home.jsx
+++ ghardhundo/src/components/Home.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import Hero from './Hero'
 import HomeCard from './HomeCard'
+import Modale from './Modale'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
-      <HomeCard/>
+      <HomeCard img='https://images.unsplash.com/photo-1512915922686-57c11dde9b6b?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      name='Tushar Towers' builder='Lodha Group' location='Thane' status='Under Construction' bhk='1BHK' price='1.5 Cr' />
+       <HomeCard
+        img='https://images.unsplash.com/photo-1610526662524-bd113e746fdb?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        name='Serene Heights'
+        builder='XYZ Developers'
+        location='Mumbai'
+        status='Ready to Move'
+        bhk='2BHK'
+        price='2.2 Cr'
+      />
+      <HomeCard
+        img='https://images.unsplash.com/photo-1597047084993-bf337e09ede0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        name='Sunset Apartments'
+        builder='ABC Builders'
+        location='Bangalore'
+        status='Under Construction'
+        bhk='3BHK'
+        price='3.5 Cr'
+      />
+      <HomeCard
+        img='https://images.unsplash.com/photo-1610402919524-dcd64aa0b17b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        name='Gardenia Residency'
+        builder='PQR Constructions'
+        location='Chennai'
+        status='Under Construction'
+        bhk='4BHK'
+        price='5.0 Cr'
+      />
+      <HomeCard
+        img='https://images.unsplash.com/photo-1533280385001-c32ffcbd52a7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        name='Hillside Retreat'
+        builder='MNO Developers'
+        location='Pune'
+        status='Ready to Move'
+        bhk='2BHK'
+        price='2.8 Cr'
+      />
+      <Modale/>
     </div>
   )
 }
---
+++ ghardhundo/src/components/Modale.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Modal } from 'antd';
+import '../css/Modal.css'
+import { Flex, Radio } from 'antd';
+
+const Modale = () => {
+    const [selectedCity, setSelectedCity] = useState("");
+    const [selectedProperty, setSelectedProperty] = useState("");
+    const handleCityChange = (e) => {
+        console.log(`Selected City: ${e.target.value}`);
+        setSelectedCity(e.target.value);
+    };
+
+    const handlePropertyChange = (e) => {
+        console.log(`Selected Property: ${e.target.value}`);
+        setSelectedProperty(e.target.value);
+    };
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
+    useEffect(() => {
+        showModal(); // Open modal when component mounts
+    }, []); 
+
+    return (
+        <div className='modalBack'>
+            {/* <Button type="primary" onClick={showModal}>
+                Open Modal
+            </Button> */}
+            <Modal title="Preference" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <div className='city'>
+                    <p className='modaltitle'>Select City: </p>
+                    <Radio.Group onChange={handleCityChange} value={selectedCity}>
+                        <Radio.Button value="Andheri">Andheri</Radio.Button>
+                        <Radio.Button value="Bandra">Bandra</Radio.Button>
+                        <Radio.Button value="Thane">Thane</Radio.Button>
+                        <Radio.Button value="Parel">Parel</Radio.Button>
+                        <Radio.Button value="Ghatkopar">Ghatkopar</Radio.Button>
+                    </Radio.Group>
+                </div>
+                <div className='modaltitle property'>
+                    <p>Select Property: </p>
+                    <Radio.Group onChange={handlePropertyChange} value={selectedProperty}>
+                        <Radio.Button value="Apartment">Apartment</Radio.Button>
+                        <Radio.Button value="Villas">Villas</Radio.Button>
+                        <Radio.Button value="Duplex">Duplex</Radio.Button>
+                        <Radio.Button value="Penthouse">Penthouse</Radio.Button>
+                    </Radio.Group>
+                </div>
+            </Modal>
+
+        </div>
+    )
+}
+
+export default Modale
